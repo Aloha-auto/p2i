c127160dd4dd595b880361e574090451d305764e
Aloha-auto <124636655+Aloha-auto@users.noreply.github.com>
2023-12-02 21:30:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
-import datetime
-import requests
-import json
-import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PIL import Image
+import requests
+import json
+import time
+import datetime
+import pytz
+import os
 
 print("imports ok")
 
@@ -21,22 +21,7 @@ IMGBB_API_KEY = os.environ['IMGBB_API_KEY'] #ImgBB
 
 places = [40, 42, 46, 43, 44, 46, 46, 42]
 
-# Fonction pour prendre une capture d'écran
-def save_screenshot(driver, filename):
-    driver.save_screenshot(filename)
-    # im = Image.open(filename)
-    # im.show()  # Ouvre la capture d'écran (commentez cette ligne si vous ne voulez pas que la capture d'écran s'affiche)
-    return filename
-
-# Fonction pour envoyer une capture d'écran à imgbb.com
-def upload_to_imgbb(api_key, image_path):
-    return None
-    url = "https://api.imgbb.com/1/upload"
-    with open(image_path, "rb") as file:
-        files = {"image": file}
-        payload = {"key": api_key}
-        response = requests.post(url, files=files, data=payload)
-        return response.json()["data"]["url"]
+tz = pytz.timezone('Europe/Paris')
 
 firefox_options = Options()
 # firefox_options.add_argument("-headless")
@@ -45,7 +30,6 @@ driver = webdriver.Firefox(options=firefox_options)
 
 print("driver created")
 
-# driver.get("https://discovery.renater.fr/edugain/WAYF?cru=yes&entityID=https%3A%2F%2Fevento.renater.fr%2F&return=https%3A%2F%2Fevento.renater.fr%2FShibboleth.sso%2FLogin%3FSAMLDS%3D1%26target%3Dss%253Amem%253Aed9904aecaee9f03359fd3b125f2fdb804586bb82705130d2733235b63904d6e")
 driver.get("https://evento.renater.fr/Shibboleth.sso/Login?target=https%3A%2F%2Fevento.renater.fr%2Fsurvey%2Ffc-ventilation-dans-les-p2i-etape-1-id32ibkl")
 
 menu = driver.find_element(By.CLASS_NAME, "select2-selection--single")
@@ -59,7 +43,6 @@ wait = WebDriverWait(driver, 10)
 time.sleep(2)
 
 search.send_keys(Keys.ENTER)
-# switch656a6cda3d7b1
 
 wait = WebDriverWait(driver, 10)
 time.sleep(2)
@@ -68,11 +51,6 @@ submit = driver.find_element(By.NAME, "Select")
 submit.submit()
 
 print("submitted renater form")
-
-# wait = WebDriverWait(driver, 10)
-# time.sleep(2)
-
-# driver.get("https://login.insa-lyon.fr/cas/login?service=https%3A%2F%2Flogin.insa-lyon.fr%2Fidp%2FAuthn%2FExtCas%3Fconversation%3De1s1&entityId=https%3A%2F%2Fevento.renater.fr%2F")
 
 WebDriverWait(driver, 10).until(EC.url_changes(driver.current_url))
 time.sleep(1)
@@ -91,11 +69,6 @@ password.submit()
 
 print("authenticated with CAS")
 
-# wait = WebDriverWait(driver, 10)
-# time.sleep(1)
-
-# driver.get("https://evento.renater.fr/survey/fc-ventilation-dans-les-p2i-etape-1-id32ibkl")
-
 WebDriverWait(driver, 10).until(EC.url_changes(driver.current_url))
 time.sleep(1)
 
@@ -112,50 +85,14 @@ try:
 except:
     pass
 
-###
-screenshot_path_before = save_screenshot(driver, "screenshot_before_switch.png")
-imgbb_url_after = upload_to_imgbb(IMGBB_API_KEY, screenshot_path_before)
-print(f"Screenshot before switch uploaded to imgbb.com: {imgbb_url_after}")
-
-###
-
 wait = WebDriverWait(driver, 10)
 
-print(f"title : {driver.title}")
-print(f"url: {driver.current_url}")
-print()
-# driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
-wait = WebDriverWait(driver, 10)
-
-# try:
-#     switch_div = driver.find_element(By.TAG_NAME, "main")
-#     print("found big div")
-#     print()
-#     print(switch_div.get_attribute("innerHTML"))
-#     print()
-#     # print(switch_div.text)
-#     # print()
-#     switch_section = switch_div.find_element(By.TAG_NAME, "section")
-#     print("found section")
-#     switch_s_div = switch_section.find_element(By.CLASS_NAME, "show-results-container")
-#     print("founs small div")
-#     switch_label = switch_s_div.find_element(By.TAG_NAME, "label")
-#     print("found label")
-#     switch_label.send_keys(Keys.ENTER)
-#     print("opened results")
-# except Exception as d:
-#     print(f"Error : {d}")
 try:
     answers_switch = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/main/section/div[2]/label")))
     answers_switch.click()
     print("switch clicked")
 except Exception as e:
     print(f"Error clicking on switch: {e}")
-    screenshot_path = save_screenshot(driver, "screenshot_error.png")
-    imgbb_url = upload_to_imgbb(IMGBB_API_KEY, screenshot_path)
-    print(f"Screenshot uploaded to imgbb.com: {imgbb_url}")
-    # Ajoutez d'autres informations de débogage si nécessaire
     raise e
 
 wait = WebDriverWait(driver, 10)
@@ -166,28 +103,18 @@ driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 wait = WebDriverWait(driver, 10)
 time.sleep(1)
 
-###
-screenshot_path_after = save_screenshot(driver, "screenshot_after_switch.png")
-imgbb_url_after = upload_to_imgbb(IMGBB_API_KEY, screenshot_path_after)
-print(f"Screenshot after switch uploaded to imgbb.com: {imgbb_url_after}")
-###
-
-
-
 answers = driver.find_elements(By.CLASS_NAME, "sum_row")[1]
 
 print("answers found")
 
-timestamp = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+timestamp = datetime.datetime.now(tz).strftime("%d/%m/%Y-%H:%M:%S")
 
 dic = {timestamp: {}}
 i = 1
 for el in answers.find_elements(By.TAG_NAME, "td") :
-    print(i)
-    print(el.text)
     dic[timestamp][i] = {"votes": int(el.text), "pourcentage": float(el.get_attribute("title").split(" : ")[1][:-1]), "dispo": max(places[i-1] - int(el.text), 0), "total": places[i-1]}
     i += 1
-print(dic)
+
 driver.close()
 print("driver closed")
 
