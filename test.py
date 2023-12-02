7f7f71c33e9bd00637c7b6299c5cb1a2774363f3
Aloha-auto <124636655+Aloha-auto@users.noreply.github.com>
2023-12-02 17:46:03 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+import requests
+import json
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from PIL import Image
+
+print("imports ok")
