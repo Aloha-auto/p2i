ead59178eff8590e382622761b79f0460e5e272e
Aloha-auto <124636655+Aloha-auto@users.noreply.github.com>
2023-12-02 17:24:32 +0100

    Update script.py

--- script.py
+++ script.py
@@ -40,12 +40,16 @@ firefox_options = Options()
 
 driver = webdriver.Firefox(options=firefox_options)
 
+print("driver created")
+
 driver.get("https://discovery.renater.fr/edugain/WAYF?cru=yes&entityID=https%3A%2F%2Fevento.renater.fr%2F&return=https%3A%2F%2Fevento.renater.fr%2FShibboleth.sso%2FLogin%3FSAMLDS%3D1%26target%3Dss%253Amem%253Aed9904aecaee9f03359fd3b125f2fdb804586bb82705130d2733235b63904d6e")
 
 menu = driver.find_element(By.CLASS_NAME, "select2-selection--single")
 menu.click()
 search = driver.find_element(By.CLASS_NAME, "select2-search__field")
 search.send_keys("INSA Lyon")
+
+print("school selected")
 
 wait = WebDriverWait(driver, 10)
 time.sleep(2)
@@ -58,6 +62,8 @@ time.sleep(2)
 
 submit = driver.find_element(By.NAME, "Select")
 submit.submit()
+
+print("submitted renater form")
 
 wait = WebDriverWait(driver, 10)
 time.sleep(2)
@@ -78,18 +84,21 @@ time.sleep(4)
 
 password.submit()
 
+print("authenticated with CAS")
+
 wait = WebDriverWait(driver, 10)
 time.sleep(1)
 
 driver.get("https://evento.renater.fr/survey/fc-ventilation-dans-les-p2i-etape-1-id32ibkl")
 
-
+print("survey launched")
 try:
     cookie_banner = driver.find_element(By.CSS_SELECTOR, 'section[data-template-content="banner_cookie_container"]')
     driver.execute_script("""
     var element = arguments[0];
     element.parentNode.removeChild(element);
     """, cookie_banner)
+    print("cookie banner deleted")
 except:
     pass
 
@@ -103,6 +112,7 @@ print(f"Screenshot before switch uploaded to imgbb.com: {imgbb_url_after}")
 try:
     answers_switch = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/main/section/div[2]/label")))
     answers_switch.click()
+    print("switch clicked")
 except Exception as e:
     print(f"Error clicking on switch: {e}")
     screenshot_path = save_screenshot(driver, "screenshot_error.png")
@@ -120,6 +130,8 @@ print(f"Screenshot after switch uploaded to imgbb.com: {imgbb_url_after}")
 ###
 
 answers = driver.find_elements(By.CLASS_NAME, "sum_row")[1]
+
+print("answers found")
 
 timestamp = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
 
